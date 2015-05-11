1fb1ec33f8184393edc3c2db23431a1dd4b6b596
Naresh Chandra Miriyala <ncmiriyala@gmail.com>
2015-05-11 22:19:52 +1000

    commit

--- src/main/java/com/dellnaresh/common/remote/PlayerService.java
+++ src/main/java/com/dellnaresh/common/remote/PlayerService.java
@@ -3,6 +3,9 @@ package com.dellnaresh.common.remote;
 import com.dellnaresh.common.remote.entities.BadimintionHire;
 import com.dellnaresh.common.remote.entities.Player;
 
+import javax.xml.ws.Response;
+import java.util.List;
+
 /**
  * Created by nareshm on 9/05/2015.
  */
@@ -10,4 +13,5 @@ public interface PlayerService {
     public void createPlayer(Player player) throws Exception;
     public void hireCourt(BadimintionHire badimintionHire) throws Exception;
     public Player getPlayer(String username) throws Exception;
+    public List<BadimintionHire> getHiredCourts(int payerId) throws Exception;
 }
